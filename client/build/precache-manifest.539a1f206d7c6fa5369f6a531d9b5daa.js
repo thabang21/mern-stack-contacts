f2de6633db83d8d9b38ed4b502ddc787edeace75
thabang21 <makuatbang@gmail.com>
2021-01-18 05:57:52 +0200

    just

---
+++ client/build/precache-manifest.539a1f206d7c6fa5369f6a531d9b5daa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b11a3941892ed82858464155d046fd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4720f63f3f9583522f8b",
+    "url": "/static/css/main.a34d5760.chunk.css"
+  },
+  {
+    "revision": "30907c58d5487f02be27",
+    "url": "/static/js/2.ef56cb3c.chunk.js"
+  },
+  {
+    "revision": "4720f63f3f9583522f8b",
+    "url": "/static/js/main.f57d4d4f.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
